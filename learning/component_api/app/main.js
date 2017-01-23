ba1f646a6a301380edd6329de96808952579ca15
Nazmul Basher <nazmul.basher@fieldnation.com>
2017-01-23 19:57:11 +0600

    Add Component Examples

--- learning/component_api/app/main.js
+++ learning/component_api/app/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App3.jsx';
+import App from './App4.jsx';
 
 ReactDOM.render(<App/>, document.getElementById('app'));
 
-setTimeout(() => {ReactDOM.unmountComponentAtNode(document.getElementById('app'));}, 10000);
+//setTimeout(() => {ReactDOM.unmountComponentAtNode(document.getElementById('app'));}, 10000);
