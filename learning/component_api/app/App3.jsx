8dd4a1acff69f52a3a4e32db2857f9697af8d922
Nazmul Basher <nazmul.basher@fieldnation.com>
2017-01-30 13:01:21 +0600

    Split in a separate components

--- learning/component_api/app/App3.jsx
+++ learning/component_api/app/App3.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Content from './Content.jsx';
 
 class App extends React.Component {
 
@@ -23,52 +24,6 @@ class App extends React.Component {
             <div>
                 <button onClick = {this.setNewNumber}>INCREMENT</button>
                 <Content myNumber = {this.state.data}></Content>
-            </div>
-        );
-    }
-}
-
-class Content extends React.Component {
-
-    componentWillMount() {
-        console.log('Component WILL MOUNT!')
-    }
-
-    componentDidMount() {
-        console.log('Component DID MOUNT!')
-    }
-
-    componentWillReceiveProps(newProps) {
-        console.log('Component WILL RECIEVE PROPS!')
-    }
-
-    shouldComponentUpdate(newProps, newState) {
-
-        if(!Object.is(newProps.myNumber, this.props.myNumber)){
-            console.log('Component SHOULD UPDATE!');
-            return true;
-        }
-
-        console.log('Component SHOULDN\'T UPDATE!');
-        return false;
-    }
-
-    componentWillUpdate(nextProps, nextState) {
-        console.log('Component WILL UPDATE!');
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        console.log('Component DID UPDATE!')
-    }
-
-    componentWillUnmount() {
-        console.log('Component WILL UNMOUNT!')
-    }
-
-    render() {
-        return (
-            <div>
-                <h3>{this.props.myNumber}</h3>
             </div>
         );
     }
