9250fd17e114b9733621079b661c435d827e0866
Nazmul Basher <nazmul.basher@fieldnation.com>
2017-02-03 18:21:09 +0600

    Added Redux DevTools

--- learning/redux_examples/todo/main.js
+++ learning/redux_examples/todo/main.js
@@ -5,7 +5,7 @@ import { createStore } from 'redux';
 import todoApp from './reducers';
 import App from './components/App';
 
-let store = createStore(todoApp);
+let store = createStore(todoApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 
 render(
