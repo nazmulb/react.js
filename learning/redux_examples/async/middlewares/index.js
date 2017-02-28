94f62e02e7d507431000fd21446eaf9504b39b69
Nazmul Basher <nazmul.basher@fieldnation.com>
2017-02-28 13:36:15 +0600

    Changed some logic

--- learning/redux_examples/async/middlewares/index.js
+++ learning/redux_examples/async/middlewares/index.js
@@ -1,6 +1,7 @@
 const logger = store => next => action => {
     console.group(action.type);
     console.info('dispatching', action);
+    console.log('prev state', store.getState());
     let result = next(action);
     console.log('new state', store.getState());
     console.groupEnd(action.type);
