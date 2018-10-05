05c3446f4374ee1e03f0ca6ca8b86fe007ee10d8
Nazmul Basher <nazmulbasher@Nazmuls-MacBook-Air.local>
2018-10-05 12:03:07 +0600

    Changed pakages and setting for webpack 4

--- learning/hello_world/webpack.config.js
+++ learning/hello_world/webpack.config.js
@@ -13,17 +13,16 @@ var config = {
     },
 
     module: {
-        loaders: [
+        rules: [
             {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-
-                query: {
-                    presets: ['es2015', 'react']
-                }
+               test: /\.jsx?$/,
+               exclude: /node_modules/,
+               loader: 'babel-loader',
+               query: {
+                  presets: ['es2015', 'react']
+               }
             }
-        ]
+         ]
     }
 }
 
