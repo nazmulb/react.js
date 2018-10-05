40543375bd257804767fa5d3b0dd8a8333791f01
Nazmul Basher <nazmulbasher@Nazmuls-MacBook-Air.local>
2018-10-05 12:22:09 +0600

    babel-preset-es2015 is deprecated so using babel-preset-env

--- learning/hello_world/webpack.config.js
+++ learning/hello_world/webpack.config.js
@@ -19,7 +19,7 @@ var config = {
                exclude: /node_modules/,
                loader: 'babel-loader',
                query: {
-                  presets: ['es2015', 'react']
+                  presets: ['env', 'react']
                }
             }
          ]
