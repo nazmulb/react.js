d9bd47631ac7bc80b65b103e32144dd03597d82e
nazmulbasher <nazmul.basher@fieldnation.com>
2020-04-12 19:46:45 +0300

    added codes

--- learning/myapp/src/Person/Person.js
+++ learning/myapp/src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from "react";
+import './Person.css'
 
 const Person = (props) => {
   return (
-    <div>
+    <div className="Person">
       <p onClick={props.click}>
         I am a {props.name} and I am {props.age} years old!
       </p>
