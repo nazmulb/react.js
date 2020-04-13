ef717893c6c6d8d1ec15a4546b573ef8dffe72a6
nazmulbasher <nazmul.basher@fieldnation.com>
2020-04-13 15:35:28 +0300

    added codes

--- learning/myapp/src/App.js
+++ learning/myapp/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Radium from "radium";
+import Radium, { StyleRoot } from "radium";
 import Person from "./Person/Person";
 import "./App.css";
 
@@ -88,14 +88,16 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(" ")}>This is really working!</p>
-        <button style={style} onClick={this.toggleHandler}>
-          Toggle Persons
-        </button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(" ")}>This is really working!</p>
+          <button style={style} onClick={this.toggleHandler}>
+            Toggle Persons
+          </button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
 }
--- learning/myapp/src/Person/Person.js
+++ learning/myapp/src/Person/Person.js
@@ -1,10 +1,16 @@
 import React from "react";
-import Radium from 'radium';
-import './Person.css'
+import Radium from "radium";
+import "./Person.css";
 
 const Person = (props) => {
+  const style = {
+    "@media (min-width: 500px)": {
+      width: "450px",
+    }
+  };
+
   return (
-    <div className="Person">
+    <div className="Person" style={style}>
       <p onClick={props.click}>
         I am a {props.name} and I am {props.age} years old!
       </p>
