617ed891219d8fe4875dbcc8e8fd4e93f4ff92d2
nazmulbasher <nazmul.basher@fieldnation.com>
2020-04-13 11:39:06 +0300

    added codes

--- learning/myapp/src/App.js
+++ learning/myapp/src/App.js
@@ -51,22 +51,9 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-          />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, "Nazmul Basher")}
-            changed={this.changeNameHandler}
-          >
-            My Hobbies: Racing
-          </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-          />
+          {this.state.persons.map((person) => (
+            <Person name={person.name} age={person.age} />
+          ))}
         </div>
       );
     }
