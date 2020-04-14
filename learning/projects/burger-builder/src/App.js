2e74f16efbf1da182ec54653b4e4d35a8fc7a710
nazmulbasher <nazmul.basher@fieldnation.com>
2020-04-14 21:46:44 +0300

    added codes

---
+++ learning/projects/burger-builder/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Layout from "./components/Layout/Layout";
+
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+        <p>My Burger</p>
+      </Layout>
+    );
+  }
+}
+
+export default App;
---
+++ learning/projects/burger-builder/src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Template from "../../hoc/Template";
+
+const Layout = (props) => (
+  <Template>
+  <div>Toolbar, SideDrawer, Backdrop</div>
+    <main>{props.children}</main>
+  </Template>
+);
+
+export default Layout;
