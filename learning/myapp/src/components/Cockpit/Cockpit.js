e32218445b3996a58565b509ce9d22a8139d5de0
nazmulbasher <nazmul.basher@fieldnation.com>
2020-04-14 15:28:59 +0300

    added codes

--- learning/myapp/src/components/Cockpit/Cockpit.js
+++ learning/myapp/src/components/Cockpit/Cockpit.js
@@ -19,10 +19,10 @@ const Cockpit = (props) => {
     btnClasses = classes.Red;
   }
 
-  if (props.persons.length <= 2) {
+  if (props.personsLength <= 2) {
     assignedClasses.push(classes.red);
   }
-  if (props.persons.length <= 1) {
+  if (props.personsLength <= 1) {
     assignedClasses.push(classes.bold);
   }
 
@@ -37,4 +37,4 @@ const Cockpit = (props) => {
   );
 };
 
-export default Cockpit;
+export default React.memo(Cockpit);
--- learning/myapp/src/containers/App.js
+++ learning/myapp/src/containers/App.js
@@ -75,7 +75,7 @@ class App extends Component {
       <div className={classes.App}>
         <Cockpit
           title={this.props.appTitle}
-          persons={this.state.persons}
+          personsLength={this.state.persons.length}
           showPersons={this.state.showPersons}
           clicked={this.toggleHandler}
         />
