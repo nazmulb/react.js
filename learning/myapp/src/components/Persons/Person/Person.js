5b3fdcf23c154236b9d14095d0efd173a82df3f3
nazmulbasher <nazmul.basher@fieldnation.com>
2020-04-14 15:32:27 +0300

    added codes

--- learning/myapp/src/components/Persons/Person/Person.js
+++ learning/myapp/src/components/Persons/Person/Person.js
@@ -9,7 +9,7 @@ const Person = (props) => {
         I am a {props.name} and I am {props.age} years old!
       </p>
       <p> {props.children} </p>
-      <input type="text" onChange={props.changed} />
+      <input type="text" value={props.name} onChange={props.changed} />
     </div>
   );
 };
