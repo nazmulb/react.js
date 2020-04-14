cd215a2fe03839081cd92ebc8c240039eb18862b
nazmulbasher <nazmul.basher@fieldnation.com>
2020-04-14 09:55:39 +0300

    added codes

--- learning/myapp/src/App.js
+++ learning/myapp/src/App.js
@@ -71,7 +71,7 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
         <p className={assignedClasses.join(" ")}>This is really working!</p>
         <button className={btnClasses} onClick={this.toggleHandler}>
