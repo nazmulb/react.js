47ebff2620c1661520b09bb260dc8a06223884f8
nazmulbasher <nazmul.basher@fieldnation.com>
2020-04-15 12:21:04 +0300

    added codes

--- learning/projects/burger-builder/src/components/Burger/Burger.js
+++ learning/projects/burger-builder/src/components/Burger/Burger.js
@@ -3,11 +3,19 @@ import classes from "./Burger.module.css";
 import Burgeringredient from "./Burgeringredient/Burgeringredient";
 
 const Burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <Burgeringredient key={igKey + i} type={igKey} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <Burgeringredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((prevVal, currentVal) => {
+      return prevVal.concat(currentVal);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
 
   return (
     <div className={classes.Burger}>
