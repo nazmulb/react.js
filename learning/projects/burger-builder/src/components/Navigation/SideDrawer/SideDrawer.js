e54119bb4a9f38068d207b35fedf06020141181b
nazmulbasher <nazmul.basher@fieldnation.com>
2020-04-15 20:21:02 +0300

    added codes

--- learning/projects/burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ learning/projects/burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,23 @@ import React from "react";
 import classes from "./SideDrawer.module.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import Template from "../../../hoc/Template";
+import Backdrop from "../../UI/Backdrop/Backdrop";
 
 const SideDrawer = (props) => {
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
-      </div>
+    <Template>
+      <Backdrop show />
+      <div className={classes.SideDrawer}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
 
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Template>
   );
 };
 
