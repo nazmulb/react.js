2e082b6cadd7feb30b75867276e2d87eea68327c
nazmulbasher <nazmul.basher@fieldnation.com>
2020-04-15 11:56:16 +0300

    added codes

--- learning/projects/burger-builder/src/components/Burger/Burger.js
+++ learning/projects/burger-builder/src/components/Burger/Burger.js
@@ -3,11 +3,16 @@ import classes from "./Burger.module.css";
 import Burgeringredient from "./Burgeringredient/Burgeringredient";
 
 const Burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <Burgeringredient key={igKey + i} type={igKey} />;
+    });
+  });
+
   return (
     <div className={classes.Burger}>
       <Burgeringredient type="bread-top" />
-      <Burgeringredient type="cheese" />
-      <Burgeringredient type="meat" />
+      {transformedIngredients}
       <Burgeringredient type="bread-bottom" />
     </div>
   );
--- learning/projects/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ learning/projects/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,19 @@ import Template from "../../hoc/Template";
 import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2,
+    },
+  };
+
   render() {
     return (
       <Template>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Template>
     );
