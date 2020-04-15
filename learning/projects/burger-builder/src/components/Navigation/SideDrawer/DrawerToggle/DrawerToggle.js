1238243a8accf3cfcdfad1e2d24a8c8a53986059
nazmulbasher <nazmul.basher@fieldnation.com>
2020-04-15 21:15:49 +0300

    added codes

--- learning/projects/burger-builder/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ learning/projects/burger-builder/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -2,7 +2,13 @@ import React from "react";
 import classes from "./DrawerToggle.module.css";
 
 const DrawerToggle = (props) => {
-  return <div className={classes.DrawerToggle} onClick={props.clicked}>MENU</div>;
+  return (
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default DrawerToggle;
