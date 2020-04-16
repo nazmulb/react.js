f7d2fc5c54ac89969609c334f714b43fcc305b86
nazmulbasher <nazmul.basher@fieldnation.com>
2020-04-16 21:04:04 +0300

    added codes

--- learning/http-blog/src/containers/Blog/Blog.js
+++ learning/http-blog/src/containers/Blog/Blog.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
 
 import classes from "./Blog.module.css";
 import Posts from "./Posts/Posts";
+import NewPost from "./NewPost/NewPost";
 
 class Blog extends Component {
   render() {
@@ -11,15 +13,17 @@ class Blog extends Component {
           <nav>
             <ul>
               <li>
-                <a href="/">Home</a>
+                <Link to="/">Home</Link>
               </li>
               <li>
-                <a href="/new-post">New Post</a>
+                <Link to="/new-post">New Post</Link>
               </li>
             </ul>
           </nav>
         </header>
-        <Posts />
+
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
       </div>
     );
   }
