b3364831f8a1ea344fabfa9b71e5dae2081f96a0
nazmulbasher <nazmul.basher@fieldnation.com>
2020-04-16 12:59:46 +0300

    added codes

--- learning/projects/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ learning/projects/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -84,7 +85,29 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("You continue!");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Nazmul Basher",
+        address: {
+          street: "12 KOS",
+          zipCode: "13401",
+          country: "USA",
+        },
+        email: "test@gmail.com",
+      },
+      deliveryMethod: "fastest",
+    };
+
+    axios
+      .post("/orders.json", order)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   render() {
