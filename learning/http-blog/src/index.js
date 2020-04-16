e1f95e6f423bf0f024194561ad020e7c25de1bb8
nazmulbasher <nazmul.basher@fieldnation.com>
2020-04-16 13:55:34 +0300

    added codes

--- learning/http-blog/src/index.js
+++ learning/http-blog/src/index.js
@@ -11,12 +11,12 @@ axios.defaults.headers.post["Content-Type"] = "application/json";
 
 // Add a request interceptor
 axios.interceptors.request.use(
-  function (config) {
+  (config) => {
     // Do something before request is sent
     console.log(config);
     return config;
   },
-  function (error) {
+  (error) => {
     // Do something with request error
     return Promise.reject(error);
   }
@@ -24,12 +24,12 @@ axios.interceptors.request.use(
 
 // Add a response interceptor
 axios.interceptors.response.use(
-  function (response) {
+  (response) => {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     return response;
   },
-  function (error) {
+  (error) => {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     return Promise.reject(error);
