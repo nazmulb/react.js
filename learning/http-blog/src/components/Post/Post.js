9af6f56028cebf5b27ac13738e628867addbd4c0
nazmulbasher <nazmul.basher@fieldnation.com>
2020-04-16 21:38:00 +0300

    added codes

--- learning/http-blog/src/components/Post/Post.js
+++ learning/http-blog/src/components/Post/Post.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { withRouter } from "react-router-dom";
 import classes from "./Post.module.css";
 
 const post = (props) => {
-  console.log(props);
   return (
     <article className={classes.Post} onClick={props.clicked}>
       <h1>{props.title}</h1>
@@ -14,4 +12,4 @@ const post = (props) => {
   );
 };
 
-export default withRouter(post);
+export default post;
