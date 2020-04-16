290903211b873515b8859cbfd45f50930c0209bf
nazmulbasher <nazmul.basher@fieldnation.com>
2020-04-16 10:56:02 +0300

    added codes

--- learning/http-blog/src/components/Post/Post.js
+++ learning/http-blog/src/components/Post/Post.js
@@ -3,9 +3,9 @@ import classes from './Post.module.css';
 
 const post = (props) => (
     <article className={classes.Post}>
-        <h1>Title</h1>
+        <h1>{props.title}</h1>
         <div className={classes.Info}>
-            <div className={classes.Author}>Author</div>
+            <div className={classes.Author}>{props.author}</div>
         </div>
     </article>
 );
--- learning/http-blog/src/containers/Blog/Blog.js
+++ learning/http-blog/src/containers/Blog/Blog.js
@@ -1,28 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import classes from './Blog.module.css';
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import classes from "./Blog.module.css";
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className={classes.Posts}>
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  state = {
+    posts: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        return res.data.slice(0, 4).map((post) => {
+          return {
+            ...post,
+            author: "Nazmul",
+          };
+        });
+      })
+      .then((posts) => {
+        this.setState({ posts });
+      });
+  }
+
+  render() {
+    const posts = this.state.posts.map((post) => {
+      return <Post key={post.id} title={post.title} author={post.author} />;
+    });
+
+    return (
+      <div>
+        <section className={classes.Posts}>{posts}</section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
