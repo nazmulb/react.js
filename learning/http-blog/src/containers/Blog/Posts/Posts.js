eb14656ca45b7c2dea99971baf10abf346d58d5c
nazmulbasher <nazmul.basher@fieldnation.com>
2020-04-17 10:56:35 +0300

    added codes

--- learning/http-blog/src/containers/Blog/Posts/Posts.js
+++ learning/http-blog/src/containers/Blog/Posts/Posts.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import axios from "../../../axios";
 
 import Post from "../../../components/Post/Post";
@@ -31,7 +30,7 @@ class Posts extends Component {
   }
 
   postSelectedHandler = (id) => {
-    this.setState({ selectedPostId: id });
+    this.props.history.push(`/${id}`);
   };
 
   render() {
@@ -40,13 +39,14 @@ class Posts extends Component {
     if (!this.state.error) {
       posts = this.state.posts.map((post) => {
         return (
-          <Link to={`/${post.id}`} key={post.id}>
-            <Post
-              title={post.title}
-              author={post.author}
-              clicked={() => this.postSelectedHandler(post.id)}
-            />
-          </Link>
+          //<Link to={`/${post.id}`} key={post.id}>
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
+          //</Link>
         );
       });
     }
