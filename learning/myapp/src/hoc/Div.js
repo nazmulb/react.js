335bbb478fa1214cee1a3ab56cd14960cfab5dcc
nazmulbasher <nazmul.basher@fieldnation.com>
2020-05-09 16:01:13 +0300

    added codes

--- learning/myapp/src/hoc/Div.js
+++ learning/myapp/src/hoc/Div.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Template = (props) => (
+const Div = (props) => (
   <div className={props.className}>{props.children}</div>
 );
 
-export default Template;
+export default Div;
